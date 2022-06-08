a94a73d778639f29fe0c773a485a786b0063d014
asim <asimmohammedqa@gmail.com>
2022-06-08 18:19:48 -0400

    new typescript

---
+++ 051722-hello-world.ts
@@ -0,0 +1,23 @@
+//const myStr: String = "Hello World!";
+//console.log(myStr);
+
+//let price = 9.95;
+//console.log(price);
+//let counter: number = 0;
+//let x : number = 100;
+//let y : number = 200; 
+//console.log(counter);
+//let firstName = 'ASIM';
+//console.log(firstName);
+//let firstName: string = 'John';
+//let title : 'web developer';
+//let profile ={''}
+//console.log(profile);
+let pending = true;
+console.log(pending);
+
+
+
+
+
+
